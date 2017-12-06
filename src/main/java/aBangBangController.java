331cb1fa50b45ef4bd26604b8a1b27a27247ec9b
Nepomuk1999 <christoph.bauer@students.fhv.at>
2017-12-06 18:49:01 +0100

    2a bangbang finish

--- src/main/java/aBangBangController.java
+++ src/main/java/aBangBangController.java
@@ -12,8 +12,8 @@ public class aBangBangController extends bangBangSuperController {
     public aBangBangController() {
         super();
         lightSensors = new LightSensor[]{getLightSensor("ls0"), getLightSensor("ls1"),
-                getLightSensor("ls3"), getLightSensor("ls4"), getLightSensor("ls5"),
-                getLightSensor("ls6"), getLightSensor("ls7")};
+                getLightSensor("ls2"), getLightSensor("ls3"), getLightSensor("ls4"),
+                getLightSensor("ls5"), getLightSensor("ls6"), getLightSensor("ls7")};
         for (LightSensor ls : lightSensors) {
             ls.enable(10);
         }
@@ -44,11 +44,11 @@ public class aBangBangController extends bangBangSuperController {
     private void checkForLight(double[] lightSensorValues) {
         for (int i = 0; i < lightSensorValues.length; i++) {
             if (i < 4) {
-                if (lightSensorValues[i] > 100) {
+                if (lightSensorValues[i] < 150) {
                     right = true;
                 }
             } else {
-                if (lightSensorValues[i] > 100) {
+                if (lightSensorValues[i] < 150) {
                     left = true;
                 }
             }
