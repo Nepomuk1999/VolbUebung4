46c8fc0ef79a02a5c79129895baecd888b204289
Nepomuk1999 <christoph.bauer@students.fhv.at>
2017-12-06 21:44:55 +0100

    a prop finish

--- src/main/java/aProportionalController.java
+++ src/main/java/aProportionalController.java
@@ -1,46 +1,49 @@
 import com.cyberbotics.webots.controller.LightSensor;
 
+import java.util.Vector;
+
 public class aProportionalController extends ProportionalSuperController {
 
-    private static int TIME_STEP = 15;
+    private static int TIME_STEP = 20;
     private LightSensor[] lightSensors;
-    //left 0,0 right 1,0
-    double[][] lightIntensity;
+    private double speedLeft;
+    private double speedRight;
+    double[][] k = {{1, 1}, {0.5, 0.5}, {-1, -1}};
+    private double maxspeed;
+
+
 
     public aProportionalController() {
         super();
-        lightSensors = new LightSensor[]{getLightSensor("ls0"), getLightSensor("ls1"),
-                getLightSensor("ls3"), getLightSensor("ls4"), getLightSensor("ls5"),
-                getLightSensor("ls6"), getLightSensor("ls7")};
-        for (LightSensor ls : lightSensors) {
-            ls.enable(10);
+        lightSensors = new LightSensor[]{getLightSensor("ls0"), getLightSensor("ls7"),
+                getLightSensor("ls2"), getLightSensor("ls5"),
+                getLightSensor("ls3"), getLightSensor("ls4")};
+        for (LightSensor l : lightSensors) {
+            l.enable(10);
         }
     }
-
 
     public void run() {
         while (step(TIME_STEP) != -1) {
-            readSensoreValues();
-            driveForward(lightIntensity[0][0], lightIntensity[1][0]);
-            resetValues();
+            evaluateSpeed();
+            drive(speedLeft, speedRight);
         }
 
     }
 
-    private void resetValues() {
-        lightIntensity[0][0] = 0;
-        lightIntensity[1][0] = 0;
+    private void evaluateSpeed() {
+        double[] sensorValues = new double[lightSensors.length];
+        for (int i = 0; i < sensorValues.length; i++) {
+            //Werte zwischen 0.0 und 1000.0 skalieren  4150 max wert des simulators nach start ohne licht
+            sensorValues[i] = (1 - (lightSensors[i].getValue() / (4150 / 3))) * (1000 / 3);
+            System.out.println("sensV" + sensorValues[i]);
+        }
+        speedLeft = (k[0][1] * sensorValues[0]) + (k[1][1] * sensorValues[2]) + (k[2][1] * sensorValues[2]);
+        speedRight = (k[0][0] * sensorValues[1]) + (k[1][0] * sensorValues[3]) + (k[2][0] * sensorValues[2]);
+        System.out.println("r" + speedRight);
+        System.out.println("l" + speedLeft);
     }
 
-    private void readSensoreValues() {
-        for (int i = 0; i <= 7; i++) {
-            if (i < 4) {
-                lightIntensity[1][0] += lightSensors[i].getValue();
-            } else {
-                lightIntensity[0][0] += lightSensors[i].getValue();
-            }
-        }
-    }
 
     public static void main(String[] args) {
         aProportionalController controller = new aProportionalController();
